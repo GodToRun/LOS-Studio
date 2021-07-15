5fb8c9ff4c4488eeca2f9cf6db77795695d15fd9
GodToRun <74130463+GodToRun@users.noreply.github.com>
2021-07-15 17:17:49 +0900

    Add files via upload

---
+++ LOSStudio/LOSStudio/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace LOSStudio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newProjectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openProjectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buildToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buildProjectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buildProjectWithBatchFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem1,
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.buildToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1904, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem1
+            // 
+            this.fileToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem});
+            this.fileToolStripMenuItem1.Name = "fileToolStripMenuItem1";
+            this.fileToolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem1.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As...";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newProjectToolStripMenuItem,
+            this.openProjectToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.fileToolStripMenuItem.Text = "Project";
+            // 
+            // newProjectToolStripMenuItem
+            // 
+            this.newProjectToolStripMenuItem.Name = "newProjectToolStripMenuItem";
+            this.newProjectToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.newProjectToolStripMenuItem.Text = "New Project";
+            this.newProjectToolStripMenuItem.Click += new System.EventHandler(this.newProjectToolStripMenuItem_Click);
+            // 
+            // openProjectToolStripMenuItem
+            // 
+            this.openProjectToolStripMenuItem.Name = "openProjectToolStripMenuItem";
+            this.openProjectToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.openProjectToolStripMenuItem.Text = "Open Project";
+            this.openProjectToolStripMenuItem.Click += new System.EventHandler(this.openProjectToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // buildToolStripMenuItem
+            // 
+            this.buildToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.buildProjectToolStripMenuItem,
+            this.buildProjectWithBatchFileToolStripMenuItem});
+            this.buildToolStripMenuItem.Name = "buildToolStripMenuItem";
+            this.buildToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.buildToolStripMenuItem.Text = "Build";
+            // 
+            // buildProjectToolStripMenuItem
+            // 
+            this.buildProjectToolStripMenuItem.Name = "buildProjectToolStripMenuItem";
+            this.buildProjectToolStripMenuItem.Size = new System.Drawing.Size(225, 22);
+            this.buildProjectToolStripMenuItem.Text = "Build Project";
+            this.buildProjectToolStripMenuItem.Click += new System.EventHandler(this.buildProjectToolStripMenuItem_Click);
+            // 
+            // buildProjectWithBatchFileToolStripMenuItem
+            // 
+            this.buildProjectWithBatchFileToolStripMenuItem.Name = "buildProjectWithBatchFileToolStripMenuItem";
+            this.buildProjectWithBatchFileToolStripMenuItem.Size = new System.Drawing.Size(225, 22);
+            this.buildProjectWithBatchFileToolStripMenuItem.Text = "Build Project with Batch File";
+            this.buildProjectWithBatchFileToolStripMenuItem.Click += new System.EventHandler(this.buildProjectWithBatchFileToolStripMenuItem_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Location = new System.Drawing.Point(7, 47);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1840, 914);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(26, 20);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "LOS Studio";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newProjectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openProjectToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.ToolStripMenuItem buildToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem buildProjectWithBatchFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripMenuItem buildProjectToolStripMenuItem;
+    }
+}
+
---
+++ LOSStudio/LOSStudio/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+namespace LOSStudio
+{
+    public partial class Form1 : Form
+    {
+        public string ProjectPath = "";
+        public string ProjectLanguage = "ASM";
+        public static Form1 instance = null;
+        private static bool Generate = false;
+        public Form1()
+        {
+            InitializeComponent();
+            instance = this;
+            if (!Generate)
+            {
+                FrontPage();
+                Generate = true;
+            }
+        }
+        public void open()
+        {
+            openProjectToolStripMenuItem_Click(null,null);
+        }
+        public void create()
+        {
+            newProjectToolStripMenuItem_Click(null,null);
+        }
+        public void FrontPage()
+        {
+            Front front = new Front();
+            front.TopMost = true;
+            front.Show();
+            front.BringToFront();
+        }
+        public void press(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 9)
+            {
+                e.Handled = false;
+            }
+        }
+        private void key_down(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.S)
+            {
+                save();
+            }
+        }
+        public void new_tab(string tabname, string filename)
+        {
+            RichTextBox box = new RichTextBox();
+            box.AcceptsTab = true;
+            box.KeyDown += new KeyEventHandler(key_down);
+            box.KeyPress += new KeyPressEventHandler(press);
+            box.Name = "text";
+            box.Size = new Size(1920,1080);
+            TabPage tab = new TabPage(tabname);
+            tab.Tag = filename;
+            tab.Controls.Add(box);
+            tabControl1.TabPages.Add(tab);
+            box.DoubleClick += new EventHandler(edit_item);
+            box.Text = File.ReadAllText(filename);
+        }
+        public void edit_item(object sender, EventArgs e)
+        {
+
+        }
+        private void RichTextBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openProjectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                ProjectPath = dialog.SelectedPath;
+                foreach (string f in Directory.GetFiles(ProjectPath))
+                {
+                    new_tab(f.Split('\\')[f.Split('\\').Length - 1], f);
+                }
+            }
+        }
+
+        private void buildProjectWithBatchFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            DialogResult result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                System.Diagnostics.Process.Start(dialog.FileName);
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            DialogResult result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                new_tab(dialog.FileName.Split('\\')[dialog.FileName.Split('\\').Length - 1], dialog.FileName);
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            save();
+        }
+        private void save()
+        {
+            RichTextBox box = (RichTextBox)tabControl1.SelectedTab.Controls["text"];
+            string path = (string)tabControl1.SelectedTab.Tag;
+            List<string> lines = new List<string>();
+            File.WriteAllLines(path, box.Lines);
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            DialogResult result = save.ShowDialog();
+            if (result != DialogResult.OK)
+                return;
+            string path = save.FileName;
+            RichTextBox box = (RichTextBox)tabControl1.SelectedTab.Controls["text"];
+            File.WriteAllLines(path, box.Lines);
+        }
+        private void AddSyntaxHighlighting(RichTextBox box)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tabControl1.TabPages.Remove(tabControl1.SelectedTab);
+        }
+
+        private void newProjectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string value = ShowDialog("","Project Path:");
+            
+            Directory.CreateDirectory(value);
+            AddConfigurationFiles(value);
+            FileStream stream = File.Create(value + "\\kernel.asm");
+            string data = @"
+            BITS 16
+
+start:
+	mov ax, 07C0h		; Set up 4K stack space after this bootloader
+	add ax, 288		; (4096 + 512) / 16 bytes per paragraph
+	mov ss, ax
+	mov sp, 4096
+
+	mov ax, 07C0h		; Set data segment to where we're loaded
+	mov ds, ax
+
+
+	mov si, text_string	; Put string position into SI
+	call print_string	; Call our string-printing routine
+
+	jmp $			; Jump here - infinite loop
+
+
+	text_string db 'This is my cool new OS!', 0
+
+
+print_string:			; Routine: output string in SI to screen
+	mov ah, 0Eh		; int 10h 'print char' function
+
+.repeat:
+	lodsb			; Get character from string
+	cmp al, 0
+	je .done		; If char is zero, end of string
+	int 10h			; Otherwise, print it
+	jmp .repeat
+
+.done:
+	ret
+
+
+	times 510-($-$$) db 0	; Pad remainder of boot sector with 0s
+	dw 0xAA55		; The standard PC boot signature";
+            stream.Close();
+            File.WriteAllText(value + "\\kernel.asm", data);
+
+            ProjectPath = value;
+            foreach (string f in Directory.GetFiles(ProjectPath))
+            {
+                new_tab(f.Split('\\')[f.Split('\\').Length - 1], f);
+            }
+        }
+        public static string ShowDialog(string text, string caption)
+        {
+            Form prompt = new Form()
+            {
+                Width = 500,
+                Height = 150,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            Label textLabel = new Label() { Left = 50, Top = 20, Text = text };
+            TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 400 };
+            Button confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(textBox);
+            prompt.Controls.Add(confirmation);
+            prompt.Controls.Add(textLabel);
+            prompt.AcceptButton = confirmation;
+
+            return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+        }
+
+        private void buildProjectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("nasm.exe", $"-f bin -o {ProjectPath + '\\'}image.bin {ProjectPath + '\\'}kernel.asm");
+            System.Diagnostics.Process.Start("cmd.exe", $"/c copy /b {ProjectPath + '\\'}image.bin {ProjectPath + '\\'}image.flp");
+            System.Diagnostics.Process.Start("cmd.exe", $"/c md {ProjectPath + '\\'}images");
+            System.Diagnostics.Process.Start("cmd.exe", $"/c copy {ProjectPath + '\\'}image.flp {ProjectPath + '\\'}images");
+            MessageBox.Show("Build successed.");
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string path = ShowDialog("","File Name(Contains path):");
+            var stream = File.Create(path);
+            stream.Close();
+            new_tab(path.Split('\\')[path.Split('\\').Length - 1], path);
+        }
+        private void AddConfigurationFiles(string path)
+        {
+            path = path + '\\';
+            var stream = File.Create(path + ".losproj");
+            stream.Close();
+        }
+    }
+}
